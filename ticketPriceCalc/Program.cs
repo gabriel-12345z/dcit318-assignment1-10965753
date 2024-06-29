d511279dffb22b4dd744c87b7a1b7eafe1414fed
gabriel-12345z <godaniels@.st.ug.edu.gh>
2024-06-29 12:37:58 -0700

    second task

---
+++ ticketPriceCalc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            Console.WriteLine("Enter age: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if(y >= 65 || y <= 12){
+                Console.WriteLine("Your ticket price is GHC7");
+            }
+            else{
+                Console.WriteLine("Your ticket price is GHC10");
+            }
+        }
+    }
+
+}
